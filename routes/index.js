fa328531a488457bcc9ba13838022ee99f8f4d15
ktaherig <k.taheri.g@protonmail.com>
2019-10-15 23:08:09 +0300

    Junior Dev blog-database issue

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const config = require('../config');
+const chalk = require('chalk');
+const sqlite3 = require('sqlite3').verbose();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/add-post', (req, res, next) => {
+	const {title, author, content} = req.body;
+	const date = new Date().toISOString();
+	const stmt = `INSERT INTO ${config.tableName} (title, date, author, post) VALUES ("${title}", "${date}", "${author}", "${content}")`;
+	db.serialize(() => {
+		db.run(stmt);
+		res.redirect('/');
+		console.log(chalk.green('Fuck yeah, d00d!'));
+	});
+	db.close();
+});
+
+module.exports = router;
